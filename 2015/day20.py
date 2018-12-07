9c618219a1a2883a3e76a453186994e486dab6db
Luke Hubmayer-Werner <mongoose41@gmail.com>
2018-12-07 12:11:43 +1030

    Fix correctness of 2015day20 part 2

--- 2015/day20.py
+++ 2015/day20.py
@@ -1,5 +1,5 @@
 import numpy as np
-max_it = 813960  # Somewhat cheaty - this value was obtained from an earlier factor-based brute force. Just use an arbitrarily large number if it fails.
+max_it = 1000000  # Arbitrarily large number, increase if it fails.
 input = 33100000
 
 houses = np.ones([max_it], dtype=np.int64)
@@ -8,8 +8,8 @@ for i in range(2, max_it):
   houses[i::i] += i
 print(np.argmax(houses > pres10))  # Part 1
 
-houses_2 = np.ones([max_it], dtype=np.int64)
+houses_2 = np.zeros([max_it], dtype=np.int64)  # Can no longer optimise out first elf
 pres11 = input//11
-for i in range(2, max_it):
+for i in range(1, max_it):
   houses_2[i:i*51:i] += i
 print(np.argmax(houses_2 > pres11))  # Part 2
