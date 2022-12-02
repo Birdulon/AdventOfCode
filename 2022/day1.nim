84ef9a5343b8eb70a2d05f5b71f6267124d952d4
Luke Hubmayer-Werner <mongoose41@gmail.com>
2022-12-02 14:52:28 +1030

    Economize parens in Nim as a joke

--- 2022/day1.nim
+++ 2022/day1.nim
@@ -4,14 +4,14 @@ import sugar
 from algorithm import sorted
 
 let inputString = strip readFile "day1-input"
-let elfStrings = inputString.split("\n\n")
-let elfRations = collect(newSeq):
-    for elf in elfStrings: collect(newSeq):
-        for c in elf.split(): parseInt(c)
-let elfCalories = collect(newSeq):
+let elfStrings = split(inputString, "\n\n")
+let elfRations = collect newSeq:
+    for elf in elfStrings: collect newSeq:
+        for c in split elf: parseInt c
+let elfCalories = collect newSeq:
     for rations in elfRations: rations.foldl(a + b)
 
 # Part 1
 echo max elfCalories
 # Part 2
-echo elfCalories.sorted()[^3 .. ^1].foldl(a + b)
+echo elfCalories.sorted[^3 .. ^1].foldl(a + b)
