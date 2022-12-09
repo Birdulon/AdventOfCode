c676a77fb503dba7cf44805ff5f883d3e2c88b8f
Luke Hubmayer-Werner <mongoose41@gmail.com>
2022-12-09 18:42:16 +1030

    Update helpers, add automatic input downloading

--- 2022/helpers.py
+++ 2022/helpers.py
@@ -1,27 +1,42 @@
-import numpy as np
 from numpy.typing import ArrayLike
+import browser_cookie3
+import datetime
+import numpy as np
 import re
+import requests
+
+today = datetime.date.today()
+day = today.day
+year = today.year
+def download_input(day: int = day):
+	filename = f'input/{day:02}'
+	r = requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=browser_cookie3.firefox())
+	if r.status_code != 200:
+		print(r)
+	else:
+		with open(filename,'w') as f:
+			f.write(r.text)
+
 numbers_pattern = re.compile(r'((?:(?<!\d)-)?\d+)')
 
-def line_to_numbers(line):
+def line_to_numbers(line: str) -> list[int]:
 	return [int(x) for x in numbers_pattern.findall(line)]
 
-def lines_to_numbers(lines):
+def lines_to_numbers(lines: list[str]) -> list[list[int]]:
 	return [line_to_numbers(line) for line in lines]
 
-def transpose_array_of_strings(aos, reverse_x = False, reverse_y = False, strip = ''):
+def transpose_array_of_strings(aos: list[str], reverse_x = False, reverse_y = False, strip = '') -> list[str]:
 	return [''.join(l[i] for l in aos[::-1 if reverse_y else 1]).strip(strip) for i in range(len(aos[0]))[::-1 if reverse_x else 1]]
 
-def read_day(day):
+def read_day(day: int) -> str:
 	with open(f'input/{day:02}', 'r') as file:
 		return file.read().strip()
 
 dtype = np.int32
-
 directions_array = np.array([[1,0], [-1,0], [0,1], [0,-1]], dtype=dtype)
 directions_dict = {'R': directions_array[0], 'L': directions_array[1], 'D': directions_array[2], 'U': directions_array[3]}  # Positive down for visualization
 
-def visualise_sparse_cells_set(cells: set, size=20, sym_hit='#', sym_miss='.'):
+def visualise_sparse_cells_set(cells: set[tuple[int, int]], size=20, sym_hit='#', sym_miss='.'):
 	vis_lol = [[sym_hit if (x,y) in cells else sym_miss for x in range(-size, size)] for y in range(-size, size)]
 	vis_str = '\n'.join(''.join(line) for line in vis_lol)
 	print(vis_str)
